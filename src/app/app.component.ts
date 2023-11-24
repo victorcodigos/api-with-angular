6ebf93dd0e7c922c9273c619b3c92230afd78769
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-11-24 12:58:31 +0100

    Learning more about angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from "./components/product/product.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, ProductComponent]
 })
 export class AppComponent {
   title = 'api-with-angular';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
+   
     
   ],
   imports: [
     BrowserModule,
+    HttpClientModule
 
   ],
   providers: [],
