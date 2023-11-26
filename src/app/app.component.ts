a3e112428a52c099cd5acc7dcd1aa71f38fbc17e
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-11-26 23:18:28 +0100

    Learning more about angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ProductComponent } from "./components/product/product.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, ProductComponent]
+    imports: [CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'api-with-angular';
