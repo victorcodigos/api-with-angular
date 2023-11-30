ce68a00bdd2e9d672a6a84f9c60f8b734bbe342e
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-11-30 14:56:19 +0100

    added console.log to see in the consola

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
   favouriteFramework: string = '';
-  
+  contactForm = new FormGroup ({
+    name: new FormControl(''),
+    email: new FormControl('')
+
+  })
 
   sendForm() {
     console.log(this.favouriteFramework)
+  }
+
+  handleContactFormSubmit() {
+    console.log(this.contactForm.value)
   }
 
 }
