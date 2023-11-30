123d972483e0775451834a7209a677f5ef20fe2d
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-11-30 11:47:31 +0100

    New component has been created and SPA as well

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [{path: 'blog/post', component: BlogComponent}];
+export const routes: Routes = [{path: 'blog/post', component: BlogComponent},{path: 'contact', component: ContactComponent}, ];
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  favouriteFramework: string = '';
+
+}
